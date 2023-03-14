fa852e8fa08403e44fe24dd9fb82e17bee311827
yu-hasse <yhsgw8023@gmail.com>
2023-03-14 20:33:59 +0900

    test add

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -53,7 +54,7 @@ func TestList(t *testing.T) {
 	recoder := httptest.NewRecorder()
 	setupRouter().ServeHTTP(recoder, request)
 
-	if err := mock.ExpectationsWereMet(); err != nil{
+	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Fatal(err.Error())
 	}
 
@@ -61,7 +62,7 @@ func TestList(t *testing.T) {
 	defer response.Body.Close()
 
 	var responseTodos []Todo
-	if err := json.NewDecoder(response.Body).Decode(&responseTodos); err != nil{
+	if err := json.NewDecoder(response.Body).Decode(&responseTodos); err != nil {
 		t.Fatal(err.Error())
 	}
 
@@ -87,5 +88,59 @@ func TestList(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
+	_gormDB, mock, err := setupDB()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	gormDB = _gormDB
+
+	expect := Todo{
+		Author:      "author",
+		Name:        "name",
+		Description: "description",
+		DueDate:     time.Now(),
+		Model:       gorm.Model{ID: 1},
+	}
+
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO `todos` (`created_at`,`updated_at`,`deleted_at`,`author`,`name`,`description`,`due_date`,`id`) VALUES (?,?,?,?,?,?,?,?)").WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), expect.Author, expect.Name, expect.Description, sqlmock.AnyArg(), sqlmock.AnyArg()).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	b, err := json.Marshal(expect)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhhost:8080/todo", bytes.NewBuffer(b))
+	recoder := httptest.NewRecorder()
+	setupRouter().ServeHTTP(recoder, request)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	response := recoder.Result()
+	defer response.Body.Close()
+
+	var responseTodo Todo
+	if err := json.NewDecoder(response.Body).Decode(&responseTodo); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if responseTodo.ID != expect.ID {
+		t.Errorf("got ID = %d. want = %d", responseTodo.ID, expect.ID)
+	}
+	if responseTodo.Author != expect.Author {
+		t.Errorf("got Author = %s. want = %s", responseTodo.Author, expect.Author)
+	}
+	if responseTodo.Name != expect.Name {
+		t.Errorf("got Name = %s. want = %s", responseTodo.Name, expect.Name)
+	}
+	if responseTodo.Description != expect.Description {
+		t.Errorf("got Description = %s. want = %s", responseTodo.Description, expect.Description)
+	}
+	if responseTodo.DueDate.Unix() != expect.DueDate.Unix() {
+		t.Errorf("got Description = %d. want = %d", responseTodo.DueDate.Unix(), expect.DueDate.Unix())
+	}
 
 }
